63b7a691d8d7263f1747b664a6bcb65718494861
Ayush Pandey <2300520100022@ietlucknow.ac.in>
2025-07-23 18:20:26 +0530

    Update ProblemPage.jsx

--- Frontend/src/component/ProblemPage.jsx
+++ Frontend/src/component/ProblemPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Editor from "@monaco-editor/react";
-import { toast } from "react-hot-toast";
+import  toast  from "react-hot-toast";
 
 const ProblemPage = () => {
   const { id } = useParams();
